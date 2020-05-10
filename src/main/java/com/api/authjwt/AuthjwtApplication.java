e0468d0988646337f82fbd9eb784ce225a6d5973
azhwani <azhwani@devwithus.com>
2020-05-10 22:36:51 +0100

    first commit

---
+++ src/main/java/com/api/authjwt/AuthjwtApplication.java
@@ -0,0 +1,13 @@
+package com.api.authjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/authjwt/AuthjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.authjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
